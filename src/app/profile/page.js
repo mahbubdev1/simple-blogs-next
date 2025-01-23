19b515b1e9192b70a06f52c01cb297afa0ef65aa
Mahbub8340 <mdmahbubalam573125@gmail.com>
2025-01-24 00:26:00 +0600

    title change

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,9 +1,8 @@
-import React from 'react';
 
 const Profile = () => {
     return (
         <div className='py-12 text-center'>
-            <h2 className='text-3xl lg:font-bold'>Hello, Welcome to your profile </h2>
+            <h2 className='text-3xl lg:font-bold'>Hello, Welcome to your profile</h2>
         </div>
     );
 };
